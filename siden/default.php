6d0012983648f656aa2f350b3a0725b62b15ddc2
unknown <willir0407@gmail.com>
2018-09-20 04:37:32 +0200

    Begynnelse på Default.php (2 bokser)

--- siden/default.php
+++ siden/default.php
@@ -1,17 +1,41 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+ <head>
     <meta charset="utf-8">
-    <title></title>
-  </head>
+    <title>Default</title>
+    <style>
+
+        #DefaultBoxes{
+          margin-top:5%;
+        }
+
+        #DefaultBoxes .Box{
+          float:left;
+          text-align: center;
+          width:44%;
+          padding:10px;
+        }
+    </style>
+
+ </head>
   <body>
-    <?php
-	     include_once 'header.php';
-    ?>
+    <h1>H1</h1>
+    <!-- Header (Nav) -->
+    <nav class="Navbar"></nav>
 
+    <section id="DefaultBoxes">
+            <div class="Box"> <!-- Alumni Overskrift (Venstre) -->
+                <h3>Sett in noe vil ha her</h3>
+                <p> ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay</p>
+            </div>
+            <div class="Box"> <!-- Registrerings box -->
+                <h3>Sett in noe vil ha her</h3>
+                <p> ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay ay </p>
+            </div>
+    </div>
 
-    <?php
-	     include_once 'footer.php';
-    ?>
+    <!-- Footer -->
+    <footer class ="footer"></footer>
+
   </body>
 </html>
