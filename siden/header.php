b37d5466b335494e4bbf3387f8396d22c7c5fec9
lyse89 <simenlyse@gmail.com>
2018-09-25 14:07:26 +0200

    fjernet body fra header.php

--- siden/header.php
+++ siden/header.php
@@ -2,9 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/Style.css">
-  <body>
-
+  <link rel="stylesheet" type="text/css" href="css/style.css">
     <div class="headerNav" id="headerNav">
       <img class="logo-headerNav" src="img/logo.png" alt="Logoen til USN" width="84" height="42">
         <div class="Loggin">
@@ -13,7 +11,4 @@
           <button type="submit">Login</button>
         </div>
     </div>
-
-
-  </body>
 </html>
