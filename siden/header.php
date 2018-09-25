43ba2953d7346e589a76c1ea78b4535a7016f9b9
lyse89 <simenlyse@gmail.com>
2018-09-25 14:04:51 +0200

    fjernet stor S fra style

--- siden/header.php
+++ siden/header.php
@@ -8,8 +8,8 @@
     <div class="headerNav" id="headerNav">
       <img class="logo-headerNav" src="img/logo.png" alt="Logoen til USN" width="84" height="42">
         <div class="Loggin">
-          <input type="text" id="navn"></label>
-          <input type="text" id="pw"></label>
+          <input type="text" placeholder="Brukernavn" id="navn">
+          <input type="text" placeholder="Passord" id="pw">
           <button type="submit">Login</button>
         </div>
     </div>
