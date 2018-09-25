1cfb1e667bd0bca07e9473f6afc30d713beb1857
lyse89 <simenlyse@gmail.com>
2018-09-25 13:28:18 +0200

    Startern på CSS'en

--- siden/header.php
+++ siden/header.php
@@ -2,11 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/Style.css">
   <body>
 
     <div class="headerNav" id="headerNav">
       <img src="img/logo.png" alt="Logoen til USN" width="84" height="42">
-        <div id="Loggin">
+        <div class="Loggin">
           <label>Brukernavnet <input type="text" id="navn"></label>
           <label>Passord <input types="password" id="pw"></label>
         </div>
