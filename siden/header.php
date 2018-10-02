0db160880e3a0bb4815395a30664951e2d7e628b
lyse89 <simenlyse@gmail.com>
2018-10-02 11:56:25 +0200

    Prøver å få center loggo

--- siden/header.php
+++ siden/header.php
@@ -1,4 +1,4 @@
-
+  <div class="navHeader">
     <div class="headerNav" id="headerNav">
       <img class="logo-headerNav" src="img/logo.png" alt="Logoen til USN" width="84" height="42">
         <div class="Loggin">
@@ -7,3 +7,4 @@
           <button type="submit">Login</button>
         </div>
     </div>
+  </div>
