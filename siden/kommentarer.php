764a6a1cc2328975a925ae188789c4707d68286f
Sorensen <mr.sorensen.k@gmail.com>
2018-10-02 15:26:38 +0200

    Lagt til css og html for kommentarer html for kommentarene må genereres med et php script som henter data fra db, men har lagt til midlertidig html-kode for testing. Mangler: Responsivt og testing med flexbox.

--- siden/kommentarer.php
+++ siden/kommentarer.php
@@ -7,6 +7,7 @@
                 margin: 0;
                 padding: 0;
                 background-color: #ddd;
+                font-family: arial;
             }
 
             .nyKommentarBoks {
@@ -35,6 +36,36 @@
                 clear: both;
                 float: right;
             }
+/* ##################################################################### */
+
+            .KommentarContainer {
+                margin: 30px 20% 20px 20%;
+                background-color: white;
+                overflow: auto;
+            }
+            .KommentarTekst {
+                margin-left: 30px;
+                float:left;
+            }
+            .Kommentar {
+                margin: 20px 50px 30px 50px;
+                overflow: auto;
+            }
+            .Kommentar img {
+                float: left;
+            }
+
+            .Kommentar ol {
+                margin: 0 0 0 0;
+                padding: 0 0 0 0;
+                list-style-type: none; 
+            }
+
+            .Kommentar ol li{
+                display: inline-block;
+                padding: 0 10px 0 0;
+                
+            }
 
         </style>
     </head>
@@ -46,7 +77,42 @@
             <button type="submit">Legg til</button>
         </div>
 
-        <div class="Kommentar">
+        <div class="KommentarContainer">
+            <div class="Kommentar">
+                <img src="img/profile-icon.png" alt="profil-bilde" width="100px">
+                <div class="KommentarTekst">
+                    <ol>
+                        <li class="kommentarNavn">Kristoffer Sorensen<li>
+                        <li class="kommentarDato">2018-09-28<li>
+                    </ol>
+<pre>
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+</pre>
+                </div>
+            </div>
         </div>
+        <div class="KommentarContainer">
+            <div class="Kommentar">
+                <img src="img/profile-icon.png" alt="profil-bilde" width="100px">
+                <div class="KommentarTekst">
+                    <ol>
+                        <li class="kommentarNavn">Kristoffer Sorensen<li>
+                        <li class="kommentarDato">2018-09-28<li>
+                    </ol>
+<pre>
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+Her er en preformatert kommentar.
+</pre>
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
