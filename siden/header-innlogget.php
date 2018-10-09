f595cbc5a5b21ecfb02135232615e2b66ef44fc8
lyse89 <simenlyse@gmail.com>
2018-10-09 11:33:03 +0200

    la inn linker

--- siden/header-innlogget.php
+++ siden/header-innlogget.php
@@ -1,6 +1,8 @@
   <div class="navHeader">
     <div class="headerNav" id="headerNav">
       <img class="logo-headerNav" src="img/logo.png" alt="Logoen til USN" width="84" height="42">
-        
+      <a href="#anonser">Anonser</a>
+      <a href="#events">Sosialt</a>
+      <a href="#nyheter">IT nyheter</a>
     </div>
   </div>
