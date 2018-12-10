c083e5d78e98775104c172c4dd61a87a375f7cb9
Sorensen <mr.sorensen.k@gmail.com>
2018-12-10 21:00:00 +0100

    Delt opp logginn-siden og php-script, fikset en del bugs, men fungerer fortsatt ikke

--- siden/logg_inn/logg_inn.php
+++ siden/logg_inn/logg_inn.php
@@ -1,7 +1,6 @@
 <?php
-
     // For aa faa variabelen $salt $dbBrukernavn $dbPassord
-    include_once("/../includes/init.php");
+    include_once("../includes/init.php");
 
     // Maa endres til vaar server / localhost for lokal jobbing
     $dsn = "mysql:host=localhost;dbname=alumni05";
@@ -20,7 +19,7 @@
             if (!$db) {die("Kunne ikke connecte til Innocent");}
 
             //  PDO prepared metode 
-            $sql = "select * from brukere where BrukerID=:br and Passord=:po";
+            $sql = "select * from bruker where brukerNavn=:br and passord=:po";
             $sth = $db->prepare($sql);
 
             // Dobbel saltet og hashet passord
@@ -32,7 +31,9 @@
             $res = $sth->fetchAll();
 
             if ($res) {
-                Header("Location: ..\side_OK.html");
+                // Header("Location: ..\side_OK.html");
+                // Hva som skal skje dersom brukeren finnes
+                echo("brukeren finnes");
             } else {
 
                 // Informasjon til bruker om at brukerId ikke finnes
@@ -40,33 +41,4 @@
             }
         }
     }
-
-
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Language" content="no-bok">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>Logg inn</title>
-</head>
-<body>
-<!-- Action maa endres til riktig fil -->
-<form method="post" id="form1" action="logginn_PDO_5.php">
-<h1 align="center">Du må logge inn for å få se den sida!</h1>
-	<table border="0" cellpadding="3" cellspacing="0" style="border-collapse: collapse" width="100%" id="table1" bgcolor="#ccFFFF">
-		<tr>
-			<td width="50%" align="right" style="padding:2 10 2 10;">Bruker:</td>
-			<td align="left" valign="top">
-        <input type="text" id="Bruker" name="brukernavn" size="20" autofocus></td>
-		</tr>
-		<tr>
-			<td width="50%" align="right" style="padding:2 10 2 10;">Passord:</td>
-			<td align="left" valign="top">
-        <input type="password" id="Passord" name="passord" size="20"></td>
-		</tr>
-	</table>
-	<p align="center"><input type="submit" value="Logg inn" name="Logginn" id="Logginn"></p>
-</form>
-<span ID="Melding"><?php echo($Melding); ?></span>
-</body>
-</html>
