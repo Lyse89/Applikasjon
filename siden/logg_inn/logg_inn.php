325b95cf5314b43120ce5ed5143d91fb01239e39
Sorensen <mr.sorensen.k@gmail.com>
2018-12-10 20:18:50 +0100

    Endret filen til aa koble seg til localhost og riktig db-navn

--- siden/logg_inn/logg_inn.php
+++ siden/logg_inn/logg_inn.php
@@ -1,46 +1,46 @@
 <?php
 
-//    // For aa faa variabelen $salt
-//    include_once("init.php");
-//
-//    // Maa endres til vaar server / localhost for lokal jobbing
-//    $dsn = "mysql:host=158.36.139.21;dbname=innocent";
-//
-//    // Maa endres fra innocent, finne ut hva disse gjor
-//    $db = new PDO($dsn,"Innocent","innocent");
-//
-//    $Melding = "";
-//    if (isSet($_POST['Logginn']) and $_POST['Logginn'] == "Logg inn") {
-//
-//        if ($_POST['brukernavn'] == "" or $_POST['passord'] == "") {
-//            $Melding = "Angi bruker og passord før du forsøker å logge inn.";
-//        } else {
-//
-//            // Denne kan ikke si "Innocent"
-//            if (!$db) {die("Kunne ikke connecte til Innocent");}
-//
-//            //  PDO prepared metode 
-//            $sql = "select * from brukere where BrukerID=:br and Passord=:po";
-//            $sth = $db->prepare($sql);
-//
-//            // Dobbel saltet og hashet passord
-//            $passord = sha1($salt.sha1($salt.&_POST['passord']));
-//
-//            $sth->bindValue(':br', $_POST['brukernavn']);
-//            $sth->bindValue(':po', $passord);
-//            $sth->execute();
-//            $res = $sth->fetchAll();
-//
-//            if ($res) {
-//                Header("Location: ..\side_OK.html");
-//            } else {
-//
-//                // Informasjon til bruker om at brukerId ikke finnes
-//                $Melding = "<p>Finner ikke BrukerID</p>";
-//            }
-//        }
-//    }
-//
+    // For aa faa variabelen $salt $dbBrukernavn $dbPassord
+    include_once("/../includes/init.php");
+
+    // Maa endres til vaar server / localhost for lokal jobbing
+    $dsn = "mysql:host=localhost;dbname=alumni05";
+
+    // Maa endres fra innocent, finne ut hva disse gjor
+    $db = new PDO($dsn,"$dbBrukernavn","dbPassord");
+
+    $Melding = "";
+    if (isSet($_POST['Logginn']) and $_POST['Logginn'] == "Logg inn") {
+
+        if ($_POST['brukernavn'] == "" or $_POST['passord'] == "") {
+            $Melding = "Angi bruker og passord før du forsøker å logge inn.";
+        } else {
+
+            // Denne kan ikke si "Innocent"
+            if (!$db) {die("Kunne ikke connecte til Innocent");}
+
+            //  PDO prepared metode 
+            $sql = "select * from brukere where BrukerID=:br and Passord=:po";
+            $sth = $db->prepare($sql);
+
+            // Dobbel saltet og hashet passord
+            $passord = sha1($salt.sha1($salt.&_POST['passord']));
+
+            $sth->bindValue(':br', $_POST['brukernavn']);
+            $sth->bindValue(':po', $passord);
+            $sth->execute();
+            $res = $sth->fetchAll();
+
+            if ($res) {
+                Header("Location: ..\side_OK.html");
+            } else {
+
+                // Informasjon til bruker om at brukerId ikke finnes
+                $Melding = "<p>Finner ikke BrukerID</p>";
+            }
+        }
+    }
+
 
 ?>
 <html>
