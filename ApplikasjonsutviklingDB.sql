ce98582a1a5f00666498654477863be4cfde0f69
Sorensen <mr.sorensen.k@gmail.com>
2018-12-10 18:53:31 +0100

    Endret db-navn til det gitte navnet for gruppen, lagt til oppretting av brukere

--- ApplikasjonsutviklingDB.sql
+++ ApplikasjonsutviklingDB.sql
@@ -1,9 +1,12 @@
 -- Applikasjonsutvikling Database --
 -- Oppretter Database  --
 -- Databasen laget og kontrollert av William Rastad --
-DROP SCHEMA IF EXISTS ApplikasjonsutviklingDB;
-CREATE SCHEMA ApplikasjonsutviklingDB;
-USE ApplikasjonsutviklingDB;
+-- Oppdatert til riktig database-navn for gruppe av Kristoffer Sorensen --
+-- Lagt til oppretting av brukere av Kristoffer Sorensen
+
+DROP SCHEMA IF EXISTS alumni05;
+CREATE SCHEMA alumni05;
+USE alumni05;
 
 -- Oppretter Tabellen bruker --
 CREATE TABLE bruker(
@@ -20,3 +23,14 @@ FeilIP 				VARCHAR(45),
 
 CONSTRAINT idbrukerPK PRIMARY KEY (idbruker)
 );
+
+-- Oppretting av admin-bruker
+CREATE USER alumni05_adm IDENTIFIED BY adm_5_aLumNi;
+GRANT ALL PRIVILEGES ON alumni05 TO alumni05_adm;
+
+-- Oppretting av bruker for applikasjon
+CREATE USER alumni_05 IDENTIFIED BY alumni;
+GRANT SELECT ON alumni05; TO alumni_05;
+GRANT INSERT ON alumni05; TO alumni_05;
+GRANT UPDATE ON alumni05; TO alumni_05;
+GRANT DELETE ON alumni05; TO alumni_05;
