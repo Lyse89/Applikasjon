1b0debfeb8d283709568dc311a81608a505e0432
Sorensen <mr.sorensen.k@gmail.com>
2018-12-10 21:41:39 +0100

    Endret brukerdata til aa ha en hash for aa teste krypteringen

--- inndata_for_testing.sql
+++ inndata_for_testing.sql
@@ -1,5 +1,7 @@
--- Laget og kontrollert av Kristoffer Sørensen
+-- Testet og kontrollert av Kristoffer Sorensen
+
 
 -- Testverdi (en bruker av systemet som skal kunne logge seg inn
+-- attributtet for passord er hashen for passordet 'passord'
 INSERT INTO bruker (brukerNavn, passord, fornavn, etternavn, fødselsdato, ePost, feilLoginnTeller, feilLoginnSiste, FeilIP)
-VALUES("bruker", "passord", "fornavn", "etternavn", curdate(), "hallo@epost.no", 1, curdate(), "FeilLoggInnIP-felt");
+VALUES("bruker", "535961d8695ee56b5d8ee42fa76063a03ac726c6", "fornavn", "etternavn", curdate(), "hallo@epost.no", 1, curdate(), "FeilLoggInnIP-felt");
