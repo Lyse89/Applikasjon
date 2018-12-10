6bdc5f64e35c063772d26113a903fd4f200d6742
lyse89 <simenlyse@gmail.com>
2018-12-10 17:33:57 +0100

    fjernet noe shit

--- siden/forsiden/registrering.inc.php
+++ siden/forsiden/registrering.inc.php
@@ -4,12 +4,12 @@ include_once 'db.inc.php';
 
 if(isset($_POST['btnSignup_form']))
 {
-   $bfornavn = trim($_POST['fornavn']);
-   $uetternavn = trim($_POST['etternavn']);
-   $epost = trim($_POST['epost']);
-   $fødselsdato = trim($_POST['fødselsdato']);
-   $passord = trim($_POST['passord']);
-   $studentid = trim($_POST['studentid']);
+   $bfornavn = $_POST['fornavn'];
+   $uetternavn = $_POST['etternavn'];
+   $epost = $_POST['epost'];
+   $fødselsdato = $_POST['fødselsdato'];
+   $passord = $_POST['passord'];
+   $studentid = $_POST['studentid'];
 
    // Dobbel saltet og hashet passord
    $passord = sha1($salt.sha1($salt.&_POST['passord']));
