c142b4f5697e7c507372f7a229beb5632d7b1450
Sorensen <mr.sorensen.k@gmail.com>
2018-12-10 21:42:47 +0100

    Naa skal logg_inn_siden fungere med hashing, salting, kryptering og all funksjonalitet mot databasen

--- siden/logg_inn/logg_inn.php
+++ siden/logg_inn/logg_inn.php
@@ -1,29 +1,38 @@
 <?php
     // For aa faa variabelen $salt $dbBrukernavn $dbPassord
     include_once("../includes/init.php");
+    echo("hallo test");
 
     // Maa endres til vaar server / localhost for lokal jobbing
     $dsn = "mysql:host=localhost;dbname=alumni05";
 
     // Maa endres fra innocent, finne ut hva disse gjor
-    $db = new PDO($dsn,"$dbBrukernavn","dbPassord");
+    $db = new PDO($dsn,"$dbBrukernavn","$dbPassord");
 
     $Melding = "";
+    if ($_POST['Logginn'] == "Logg inn") {
+        echo("ja");
+    }
+
+    echo($_POST['Logginn'] == "Logg inn");
+    echo($_POST['brukernavn']);
+    echo("\n");
     if (isSet($_POST['Logginn']) and $_POST['Logginn'] == "Logg inn") {
+
 
         if ($_POST['brukernavn'] == "" or $_POST['passord'] == "") {
             $Melding = "Angi bruker og passord før du forsøker å logge inn.";
         } else {
 
-            // Denne kan ikke si "Innocent"
-            if (!$db) {die("Kunne ikke connecte til Innocent");}
+            if (!$db) {die("Kunne ikke connecte til Databasen");}
 
             //  PDO prepared metode 
             $sql = "select * from bruker where brukerNavn=:br and passord=:po";
             $sth = $db->prepare($sql);
 
             // Dobbel saltet og hashet passord
-            $passord = sha1($salt.sha1($salt.&_POST['passord']));
+            $passord = sha1($salt.sha1($salt.$_POST['passord']));
+            echo($passord);
 
             $sth->bindValue(':br', $_POST['brukernavn']);
             $sth->bindValue(':po', $passord);
