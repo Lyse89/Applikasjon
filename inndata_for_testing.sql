79dc569a96f25b81b3b665dcc85388da7c34b661
PastaSauce <willir0407@gmail.com>
2018-12-11 14:41:57 +0100

    DB updated

--- inndata_for_testing.sql
+++ inndata_for_testing.sql
@@ -3,5 +3,5 @@
 
 -- Testverdi (en bruker av systemet som skal kunne logge seg inn
 -- attributtet for passord er hashen for passordet 'passord'
-INSERT INTO bruker (brukerNavn, passord, fornavn, etternavn, fødselsdato, ePost, feilLoginnTeller, feilLoginnSiste, FeilIP)
-VALUES("bruker", "535961d8695ee56b5d8ee42fa76063a03ac726c6", "fornavn", "etternavn", curdate(), "hallo@epost.no", 1, curdate(), "FeilLoggInnIP-felt");
+INSERT INTO bruker (brukerNavn, passord, fornavn, etternavn, ePost, feilLoginnTeller, feilLoginnSiste, FeilIP)
+VALUES("bruker", "535961d8695ee56b5d8ee42fa76063a03ac726c6", "fornavn", "etternavn", "hallo@epost.no", 1, curdate(), "FeilLoggInnIP-felt");
