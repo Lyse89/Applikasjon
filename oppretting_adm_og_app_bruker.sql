eba3c325d72cca91312c003552654eb77ee4297f
Sorensen <mr.sorensen.k@gmail.com>
2018-12-11 15:39:49 +0100

    Lagt til drop user if exists i sql script

--- oppretting_adm_og_app_bruker.sql
+++ oppretting_adm_og_app_bruker.sql
@@ -1,10 +1,12 @@
 -- Laget og kontrollert av Kristoffer Sørensen
 
 -- Oppretting av admin-bruker
+DROP USER IF EXISTS alumni05_adm;
 CREATE USER alumni05_adm IDENTIFIED BY 'adm_5_aLumNi';
 GRANT ALL PRIVILEGES ON alumni05 TO alumni05_adm;
 
 -- Oppretting av bruker for applikasjon
+DROP USER IF EXISTS alumni_05;
 CREATE USER alumni_05 IDENTIFIED BY 'alumni';
 -- Usikker paa om ".*" er en grei maate aa gjore det her paa
 GRANT SELECT ON alumni05.* TO alumni_05;
