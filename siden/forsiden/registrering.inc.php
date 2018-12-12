96c30db105c7ab2649af409a16e0b4ae7cd4ce46
lyse89 <simenlyse@gmail.com>
2018-12-12 14:31:27 +0100

    fikset loggin teller

--- siden/forsiden/registrering.inc.php
+++ siden/forsiden/registrering.inc.php
@@ -4,10 +4,14 @@
 if(isset($_POST['btnSignup_form'])) {
    include_once("../includes/init.php");
    $dsn = "mysql:host=localhost;dbname=alumni05";
-   $db = new PDO($dsn,"$dbBrukernavn","$dbPassord");
+
+   // Maa endres fra innocent, finne ut hva disse gjor
+   $db = new PDO($dsn,"root","1234");
+
+   $loginnTeller = 0;
 
    $sql = "insert into bruker (brukerNavn,passord,fornavn,etternavn,ePost,feilLoginnTeller)";
-   $sql.= "values (:studentid,:passord,:fornavn,:etternavn,:epost,:loginnTeller)";
+   $sql.= "values (:studentid,:passord,:fornavn,:etternavn,:epost,$loginnTeller)";
 
    $stmt = $db->prepare($sql);
 
@@ -16,23 +20,20 @@ if(isset($_POST['btnSignup_form'])) {
    $stmt->bindParam(':epost',$bepost);
    $stmt->bindParam(':passord',$bpassord);
    $stmt->bindParam(':studentid',$bstudentid);
-   $smt->bindParam('feilLoginnTeller');
+
 
    $bfornavn = $_POST['fornavn'];
    $betternavn = $_POST['etternavn'];
    $bepost = $_POST['epost'];
    $bpassord = $_POST['passord'];
    $bstudentid = $_POST['studentid'];
-   $loginnTeller = 0;
 
-   // Dobbel saltet og hashet passord
-   // $bpassord = sha1($salt.sha1($salt.&_POST['passord']));
 
    $stmt->execute();
 
     } else {
     }
-   header("Location: /siden/");
+   // header("Location: /app/siden/");
 
 
 ?>
