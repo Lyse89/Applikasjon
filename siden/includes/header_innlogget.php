0a621d67b6e5041f8e2a5ed37b4c36bbde731329
lyse89 <simenlyse@gmail.com>
2018-12-13 16:09:03 +0100

    La til styling på logg ut button fra CSS

--- siden/includes/header_innlogget.php
+++ siden/includes/header_innlogget.php
@@ -1,7 +1,12 @@
 <nav>
-  <img class="logo-headerNav" src="../img/logo.png" alt="Logoen til USN" width="84" height="42">
+  <img class="logo-navHeaderForsiden" src="../img/logo.png" alt="Logoen til USN" width="84" height="42">
   <a href="#anonser">Anonser</a>
   <a href="#events">Sosialt</a>
   <a href="#nyheter">IT nyheter</a>
   <a href="#minSIde" right-margin="5%">Min Side</a>
+  <div class="Loggin">
+    <form action="../siden/includes/loggut.inc.php" method="POST">
+    <button type="submit" name="submit">Logout</button>
+    </form>
+  </div>
 </nav>
