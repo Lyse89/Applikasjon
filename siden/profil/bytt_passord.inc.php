11d703d3cb0e2f16681be2d6917e42cd048cf847
lyse89 <simenlyse@gmail.com>
2018-12-14 20:38:41 +0100

    Startet på bytte passord

--- siden/profil/bytt_passord.inc.php
+++ siden/profil/bytt_passord.inc.php
@@ -1,2 +1,16 @@
-<!-- Denne include-siden er utviklet av Simen A. Lyse , siste gang endret 14.12.2018
+<?php
+//-- Denne include-siden er utviklet av Simen A. Lyse , siste gang endret 14.12.2018
 // Denne include-siden er kontrollert av Simen A. Lyse, siste gang 14.12.2018 -->
+
+include_once("../includes/init.php");
+
+// Maa endres til vaar server / localhost for lokal jobbing
+$dsn = "mysql:host=localhost;dbname=alumni05";
+
+// Maa endres fra innocent, finne ut hva disse gjor
+$db = new PDO($dsn,"$dbBrukernavn","$dbPassord");
+
+$Melding = "";
+if (isSet($_POST['byttePO_form']) and $_POST['byttePO_form'] == "Bytte passord") {
+
+?>
--- siden/profil/profil.php
+++ siden/profil/profil.php
@@ -13,8 +13,8 @@
     include_once('../includes/ikke_logget_inn.inc.php');
     ?>
 
-    <a href="bytt_passord.php">Bytt passord</a>
-    <a href="#LastOppProfilBilde">Last opp profil bilde</a>
+    <a href="bytt_passord.php">Bytt passord</a><br>
+    <a href="#LastOppProfilBilde">Last opp profil bilde</a><br>
 
   </body>
 </html>
