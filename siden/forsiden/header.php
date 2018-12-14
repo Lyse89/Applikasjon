ba8240617443e4b90f13ede2cb4bad6cc075c530
Sorensen <mr.sorensen.k@gmail.com>
2018-12-14 21:53:30 +0100

    Lagt til navn for sider laget av ..

--- siden/forsiden/header.php
+++ siden/forsiden/header.php
@@ -1,4 +1,4 @@
-<!-- Denne include-siden er utviklet av Simen A. Lyse , siste gang endret 14.12.2018
+<!-- Denne include-siden er utviklet av Simen A. Lyse og Kristoffer Sorensen, siste gang endret 14.12.2018
 // Denne include-siden er kontrollert av Simen A. Lyse, siste gang 14.12.2018 -->
 <nav>
 <a class="bilde" href="default.php">
