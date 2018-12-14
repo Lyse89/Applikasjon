5e836745d9d8ed513e45c76ee1dffc611fc65bbd
Sorensen <mr.sorensen.k@gmail.com>
2018-12-14 21:09:36 +0100

    Lagt inn php-script i logg_inn_siden.php, endret pointer til script, lagt inn kommentarer

--- siden/logg_inn/logg_inn_siden.php
+++ siden/logg_inn/logg_inn_siden.php
@@ -1,6 +1,55 @@
+<!-- Denne siden er utviklet av William Rastad og Kristoffer Sorensen, siste gang endret 14.12.2018 -->
+<!-- Denne siden er kontrollert av William Rastad og Kristoffer Sorensen, siste gang 14.12.2018 -->
+
+<?php
+
+    // For aa faa variabelen $salt $dbBrukernavn $dbPassord
+    include_once("../includes/init.php");
+
+    // Maa endres til vaar server / localhost for lokal jobbing
+    $dsn = "mysql:host=localhost;dbname=alumni05";
+
+    // Maa endres fra innocent, finne ut hva disse gjor
+    $db = new PDO($dsn,"$dbBrukernavn","$dbPassord");
+
+    $Melding = "";
+    if (isSet($_POST['Logginn']) and $_POST['Logginn'] == "Logg inn") {
+
+        if ($_POST['brukernavn'] == "" or $_POST['passord'] == "") {
+            $Melding = "Angi bruker og passord f�r du fors�ker � logge inn.";
+        } else {
+
+            if (!$db) {die("Kunne ikke connecte til Databasen");}
+
+            //  PDO prepared metode
+            $sql = "select * from bruker where brukerNavn=:br and passord=:po";
+            $sth = $db->prepare($sql);
+
+            // Dobbel saltet og hashet passord
+            $passord = sha1($salt.sha1($salt.$_POST['passord']));
+
+            $sth->bindValue(':br', $_POST['brukernavn']);
+            $sth->bindValue(':po', $passord);
+            $sth->execute();
+            $res = $sth->fetchAll();
+
+            if ($res) {
+                session_start();
+                // Denne parameteren (sessionId) skal endres til loggetInn
+                $_SESSION['sessionId'] = 'sessionIdEtellerannet';
+
+                header("Location: ../innlogget_forside/innlogget_forside2.php");
+
+
+            } else {
+                // Login Failed
+                echo("Innlogging mislykket");
+                // header("Location: logg_inn_siden.php");
+            }
+        }
+    }
+?>
 <html>
-<!-- Denne siden er utviklet av William Rastad, siste gang endret 14.12.2018 -->
-<!-- Denne siden er kontrollert av William Rastad, siste gang 14.12.2018 -->
 <head>
 <meta http-equiv="Content-Language" content="no-bok">
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
@@ -27,7 +76,7 @@ input[type=password], input[type=text]{
 </nav>
 
 <!-- Action post log_inn.php-->
-<form method="post" id="form1" action="logg_inn.php">
+<form method="post" id="form1" action="logg_inn_siden.php">
 <h1 align="center">Logg inn her</h1>
 	<table border="0" cellpadding="3" cellspacing="0" style="border-collapse: collapse" width="100%" id="table1">
 		<tr>
