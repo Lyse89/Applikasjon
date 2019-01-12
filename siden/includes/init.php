fa4ca727ab179efa0563163a3daefb102a2e87d8
Sorensen <mr.sorensen.k@gmail.com>
2019-01-12 13:16:43 +0100

    Fikset dsn, fjerner gammel default.php

--- siden/includes/init.php
+++ siden/includes/init.php
@@ -2,5 +2,7 @@
     $salt = "IT2_2019";
     $dbBrukernavn = "alumni_05";
     $dbPassord = "alumni";
-    $dsn = "mysql:host=158.36.139.21;dbname=alumni05";
+    // Connection til database paa skoleserver
+    // $dsn = "mysql:host=158.36.139.21;dbname=alumni05";
+    $dsn = "mysql:host=localhost;dbname=alumni05";
 ?>
