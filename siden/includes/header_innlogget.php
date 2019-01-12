64ac2d4f8f162b1e806517841a8270828ccd9070
PastaSauce <willir0407@gmail.com>
2019-01-12 15:12:16 +0100

    Personer / søk på personer lagt til i inlogget Header

--- siden/includes/header_innlogget.php
+++ siden/includes/header_innlogget.php
@@ -4,9 +4,10 @@
   <a class="bilde" href="../../default.php">
     <img class="logo-navHeaderForsiden" src="../img/logo.png" alt="Logoen til USN" width="84" height="42">
   </a>
-  <a href="../anonser/anonser.php">Anonser</a>
+  <a href="../anonser/anonser.php">Annonser</a>
   <a href="../sosialt/sosialt.php">Sosialt</a>
   <a href="../nyheter/nyheter.php">IT nyheter</a>
+  <a href="#">Personer</a>
   <a href="../profil/profil.php">Min Side</a>
   <div class="Loggin">
     <form action="../includes/loggut.inc.php" method="POST">
