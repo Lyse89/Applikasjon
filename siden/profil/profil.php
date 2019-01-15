f31c1eaf7e33320e7a4774d8e5c9b9a4c3725544
lyse89 <simenlyse@gmail.com>
2019-01-15 11:41:53 +0100

    La til arbeidsoppgaver Kommentar i CSS for å henvise til header Opprettet bytteEpost, passord, lasteoppbilde og presentasjon scrips i profil

--- siden/profil/profil.php
+++ siden/profil/profil.php
@@ -13,8 +13,36 @@
     include_once('../includes/ikke_logget_inn.inc.php');
     ?>
 
-    <a href="bytt_passord.php">Bytt passord</a><br>
-    <a href="#LastOppProfilBilde">Last opp profil bilde</a><br>
+    <h2>Last Opp Profil Bilde</h2>
+    <form class="lasteOppProfilBilde" action"/lastebilde.inc.php" method="POST" enctype="multipart/form-data">
+      <input type="file" name="lasteOppProfilBilde" id="lasteOppProfilBilde"><br>
+      <input type="submit" value="Last Opp Bilde" name="submitProfilBilde">
+    </form>
+
+    <h2>Redigere Din Presentasjon<h2>
+    <form class="presentasjon" action="/presentasjon.inc.php" method="POST">
+      <textarea>
+      </textarea><br>
+      <input type="submit" name="submitPres" id="skrivPres" value="Redigere">
+    </form>
+
+    <h2>Registrere interesser</h2>
+
+    <h2>Bytte Passord</h2>
+    <form class="byttePO" action="/bytt_passord.inc.php" method="POST">
+        <input type="password" name="gamlePO" id=gamlePO placeholder="Gamle Passordet"><br>
+        <input type="password" name="nyePO" id=nyePO placeholder="Nye Passordet"><br>
+        <input type="password" name="nyePO_sjekk" placeholder="Gjenta ny Passordet"><br>
+        <input type="submit" name="submitByttePO" id="byttePO" value="Bytt Passord">
+    </form>
+
+    <h2>Bytte E-post</h2>
+    <form class="BytteEpost" action="/bytteEpost.inc.php" method="POST">
+      <input type="email" name="epost" placeholder="Skriv nye Epost"><br>
+      <input type="password" name="passord" placeholder="Passord"><br>
+      <input type="submit" name="submitBytteEpost" id="bytteEpost" value="Bytte Epost">
+    </form>
+
 
   </body>
 </html>
