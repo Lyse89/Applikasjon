64fccd73390edc7256b952adac70eb5b89c6c464
lyse89 <simenlyse@gmail.com>
2019-02-04 00:29:02 +0100

    Startet CSS på profil siden

--- siden/profil/lastebilde.inc.php
+++ siden/profil/lastebilde.inc.php
@@ -5,35 +5,26 @@ $LastseOpp = 1;
 if(isset($_POST["submitProfilBilde"])) {
     $Fil = $Mappe . basename($_FILES["lasteOppProfilBilde"]["name"]);
     $FileType = strtolower(pathinfo($Fil,PATHINFO_EXTENSION));
-    $Sjekk = getimagesize($_FILES["lasteOppProfilBilde"]["tmp_name"]);
-    if($Sjekk !== false) {
-        $Melding =  "File is an image - " . $Sjekk["mime"] . ".";
-        $LastseOpp = 1;
-    } else {
-        $Melding =  "Filen er ikke et bilde";
-        $LastseOpp = 0;
-    }
 }
 
 if ($_FILES["lasteOppProfilBilde"]["size"] > 500000) {
-    $Melding =  "Filen er for stor, må være mindre en 500 000 bytes";
-    $LastseOpp = 0;
+    //  "Filen er for stor, må være mindre en 500 000 bytes";
+    header("Location: profil.php?bilde=storfil");
+    exit();
 }
 
-if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg"
-&& $FileType != "gif" ) {
-    $Melding =  "Bare jpg, png, jpeg og gif er lov å laste opp.";
-    $LastseOpp = 0;
-}
+if($FileType != "jpg" && $FileType != "png" && $FileType != "jpeg") {
+    //  "Bare jpg, png, jpeg og gif er lov å laste opp.";
+    header("Location: profil.php?bilde=feiltype");
+    exit();
+    }
 
-if ($LastseOpp == 0) {
-    $Melding = " Filen kunne ikke bli lastet opp.";
 
-} else {
+else {
     if (move_uploaded_file($_FILES["lasteOppProfilBilde"]["tmp_name"], $Fil)) {
-        $Melding = "Filen ". basename( $_FILES["lasteOppProfilBilde"]["name"]). " har blitt lastet opp.";
-    } else {
-        $Melding =  "En ukjent feil skjede når du prøvde å laste opp, kontak eier av siden vis det ikke funker";
+        $CookieMelding = "Filen ". basename( $_FILES["lasteOppProfilBilde"]["name"]). " har blitt lastet opp.";
+        header("Location: profil.php?bilde=suksess");
+        exit();
     }
 }
 
--- siden/profil/profil.php
+++ siden/profil/profil.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <link rel="stylesheet" type="text/css" href="../css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
   <head>
     <meta charset="utf-8">
     <title>Profil</title>
@@ -14,28 +15,48 @@
     include_once('../includes/ikke_logget_inn.inc.php');
     ?>
 
-    <h2>Last Opp Profil Bilde</h2>
+    <section class="navside">
+      <a href="#lastoppbilde">Laste opp Bilde</a>
+      <a href="#redigerePres">Redigere Presentasjon</a>
+      <a href="#registInt">Registrere interesser</a>
+      <a href="#byttePass">Bytte passord</a>
+      <a href="#bytteEpost">Bytte epost</a>
+    </section>
+
+    <section class="siden">
+    <h2 id="lastoppbilde">Last Opp Profil Bilde</h2>
     <form action="lastebilde.inc.php" method="POST" enctype="multipart/form-data">
       Velg et profil bilde:<br>
-
+      <?php
+      $url ="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      if (strpos($url, "bilde=suksess") == true) {
+        echo "<p class='suksess'>Bilde lastet opp</p>";
+      }
+      if (strpos($url, "bilde=feiltype") == true) {
+        echo "<p class='error'>Bilde kan bare være jpg, png, og jpeg</p>";
+      }
+      if (strpos($url, "bilde=storfil") == true) {
+        echo "<p class='error'>Bilde må være under 500000 byte</p>";
+      }
+      ?>
       <input type="file" name="lasteOppProfilBilde" id="lasteOppProfilBilde"><br>
       <input type="submit" value="Last Opp Bilde" name="submitProfilBilde">
     </form>
 
-    <h2>Redigere Din Presentasjon<h2>
-    <form class="presentasjon" action="/presentasjon.inc.php" method="POST">
+    <h2 id="redigerePres">Redigere Din Presentasjon<h2>
+    <form class="presentasjon" action="presentasjon.inc.php" method="POST">
       <textarea>
       </textarea><br>
       <input type="submit" name="submitPres" id="skrivPres" value="Redigere">
     </form>
 
-    <h2>Registrere interesser</h2>
+    <h2 id="registInt">Registrere interesser</h2>
     <form class="presentasjon" action="../profil/registrer_interesse.inc.php" method="POST">
       <input type="text" name="interesser" id=interesse placeholder="Skriv en Interesse"><br>
       <input type="submit" name="registrerInt" id="registrerInt">
     </form>
 
-    <h2>Bytte Passord</h2>
+    <h2 id="byttePass">Bytte Passord</h2>
     <form class="byttePO" action="/bytt_passord.inc.php" method="POST">
         <input type="password" name="gamlePO" id=gamlePO placeholder="Gamle Passordet"><br>
         <input type="password" name="nyePO" id=nyePO placeholder="Nye Passordet"><br>
@@ -43,12 +64,13 @@
         <input type="submit" name="submitByttePO" id="byttePO" value="Bytt Passord">
     </form>
 
-    <h2>Bytte E-post</h2>
+    <h2 id="bytteEpost">Bytte E-post</h2>
     <form class="BytteEpost" action="/bytteEpost.inc.php" method="POST">
       <input type="email" name="epost" placeholder="Skriv nye Epost"><br>
       <input type="password" name="passord" placeholder="Passord"><br>
       <input type="submit" name="submitBytteEpost" id="bytteEpost" value="Bytte Epost">
     </form>
+  </section>
 
 
   </body>
