9f36c68d630d375882cf79a3e0000dfe047d25bb
unknown <willir0407@gmail.com>
2019-02-05 09:33:05 +0100

    Header, Sosialt til Events, Personer til Sok interesser

--- siden/includes/header_innlogget.php
+++ siden/includes/header_innlogget.php
@@ -5,9 +5,9 @@
     <img class="logo-navHeaderForsiden" src="../img/logo.png" alt="Logoen til USN" width="84" height="42">
   </a>
   <a href="../anonser/anonser.php">Annonser</a>
-  <a href="../sosialt/sosialt.php">Sosialt</a>
+  <a href="../sosialt/sosialt.php">Events</a>
   <a href="../nyheter/nyheter.php">IT nyheter</a>
-  <a href="../personer/personer.php">Personer</a>
+  <a href="../personer/personer.php">Søk Interesser</a>
   <a href="../profil/profil.php">Min Side</a>
   <div class="Loggin">
     <form action="../includes/loggut.inc.php" method="POST">
