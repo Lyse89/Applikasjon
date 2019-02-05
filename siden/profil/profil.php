f04f57674901f45751eb739f2be9199a68caa312
lyse89 <simenlyse@gmail.com>
2019-02-05 13:09:13 +0100

    Jvascript

--- siden/profil/profil.php
+++ siden/profil/profil.php
@@ -15,46 +15,44 @@
     include_once('../includes/ikke_logget_inn.inc.php');
     ?>
 
-    <section class="navside">
-      <a href="#lastoppbilde">Laste opp Bilde</a>
-      <a href="#redigerePres">Redigere Presentasjon</a>
-      <a href="#registInt">Registrere interesser</a>
-      <a href="#byttePass">Bytte passord</a>
-      <a href="#bytteEpost">Bytte epost</a>
-    </section>
+    <button class="profilSettings" onclick="openPage('profil', this, 'red')" id="defaultOpen">Profil</button>
+    <button class="profilSettings" onclick="openPage('pass', this, 'green')">Bytte Passord</button>
+    <button class="profilSettings" onclick="openPage('email', this, 'blue')">Bytte email</button>
 
-    <section class="siden">
+    <section id="profil" class="section">
     <h2 id="lastoppbilde">Last Opp Profil Bilde</h2>
-    <form action="lastebilde.inc.php" method="POST" enctype="multipart/form-data">
-      Velg et profil bilde:<br>
-      <?php
-      $url ="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-      if (strpos($url, "bilde=suksess") == true) {
-        echo "<p class='suksess'>Bilde lastet opp</p>";
-      }
-      if (strpos($url, "bilde=feiltype") == true) {
-        echo "<p class='error'>Bilde kan bare være jpg, png, og jpeg</p>";
-      }
-      if (strpos($url, "bilde=storfil") == true) {
-        echo "<p class='error'>Bilde må være under 500000 byte</p>";
-      }
-      ?>
-      <input type="file" name="lasteOppProfilBilde" id="lasteOppProfilBilde"><br>
-      <input type="submit" value="Last Opp Bilde" name="submitProfilBilde">
-    </form>
+      <form action="lastebilde.inc.php" method="POST" enctype="multipart/form-data">
+        Velg et profil bilde:<br>
+        <?php
+        $url ="http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        if (strpos($url, "bilde=suksess") == true) {
+          echo "<p class='suksess'>Bilde lastet opp</p>";
+        }
+        if (strpos($url, "bilde=feiltype") == true) {
+          echo "<p class='error'>Bilde kan bare være jpg, png, og jpeg</p>";
+        }
+        if (strpos($url, "bilde=storfil") == true) {
+          echo "<p class='error'>Bilde må være under 500000 byte</p>";
+        }
+        ?>
+        <input type="file" name="lasteOppProfilBilde" id="lasteOppProfilBilde"><br>
+        <input type="submit" value="Last Opp Bilde" name="submitProfilBilde">
+      </form>
 
-    <h2 id="redigerePres">Redigere Din Presentasjon<h2>
-    <form class="presentasjon" action="presentasjon.inc.php" method="POST">
-      <textarea name="bio" placeholder="Skriv din bio"></textarea><br>
-      <input type="submit" name="submitPres" id="skrivPres" value="Redigere">
-    </form>
+      <h2 id="redigerePres">Redigere Din Presentasjon<h2>
+      <form class="presentasjon" action="presentasjon.inc.php" method="POST">
+        <textarea name="bio" placeholder="Skriv din bio"></textarea><br>
+        <input type="submit" name="submitPres" id="skrivPres" value="Redigere">
+      </form>
 
-    <h2 id="registInt">Registrere interesser</h2>
-    <form class="presentasjon" action="../profil/registrer_interesse.inc.php" method="POST">
-      <input type="text" name="interesser" id=interesse placeholder="Skriv en Interesse"><br>
-      <input type="submit" name="registrerInt" id="registrerInt">
-    </form>
+      <h2 id="registInt">Registrere interesser</h2>
+      <form class="presentasjon" action="../profil/registrer_interesse.inc.php" method="POST">
+        <input type="text" name="interesser" id=interesse placeholder="Skriv en Interesse"><br>
+        <input type="submit" name="registrerInt" id="registrerInt">
+      </form>
+  </section>
 
+  <section id="pass" class="section">
     <h2 id="byttePass">Bytte Passord</h2>
     <form class="byttePO" action="/bytt_passord.inc.php" method="POST">
         <input type="password" name="gamlePO" id=gamlePO placeholder="Gamle Passordet"><br>
@@ -62,7 +60,9 @@
         <input type="password" name="nyePO_sjekk" placeholder="Gjenta ny Passordet"><br>
         <input type="submit" name="submitByttePO" id="byttePO" value="Bytt Passord">
     </form>
+  </section>
 
+  <section id="email" class="section">
     <h2 id="bytteEpost">Bytte E-post</h2>
     <form class="BytteEpost" action="/bytteEpost.inc.php" method="POST">
       <input type="email" name="epost" placeholder="Skriv nye Epost"><br>
@@ -70,6 +70,34 @@
       <input type="submit" name="submitBytteEpost" id="bytteEpost" value="Bytte Epost">
     </form>
   </section>
+
+  <script type="text/javascript">
+
+    function openPage(pageName, elmnt, color) {
+      // Hide all elements with class="tabcontent" by default */
+      var i, section, profilSettings;
+      section = document.getElementsByClassName("section");
+      for (i = 0; i < section.length; i++) {
+        section[i].style.display = "none";
+      }
+
+      // Remove the background color of all tablinks/buttons
+      profilSettings = document.getElementsByClassName("profilSettings");
+      for (i = 0; i < profilSettings.length; i++) {
+        profilSettings[i].style.backgroundColor = "";
+      }
+
+      // Show the specific tab content
+      document.getElementById(pageName).style.display = "block";
+
+      // Add the specific color to the button used to open the tab content
+      elmnt.style.backgroundColor = color;
+    }
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+
+  </script>
 
 
   </body>
