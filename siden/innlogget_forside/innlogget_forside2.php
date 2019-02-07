e42d7d0b310d7479b1e3a488725f52650ac82f46
Sorensen <mr.sorensen.k@gmail.com>
2019-02-07 20:14:32 +0100

    [inl.forside] Mer styling med css. Brede kolonner, topp-boks for nyheter, midlertidig footer.

--- siden/innlogget_forside/innlogget_forside2.php
+++ siden/innlogget_forside/innlogget_forside2.php
@@ -15,13 +15,12 @@ body {
     font-family:arial;
     background-color:#ddd;
 }
-.topp-boks {
-    height: 75px;
-}
+
 /* Endre navnet paa center */
 .center {
     box-shadow: 10px 10px 8px #c0c0c0;
-    margin: 0 5% 50px 5%;
+    margin: 0 0 50px 0;
+    padding: 0 5% 0 5%;
     width: 90%;
     /*max-width: 1230px;*/
     background-color: white;
@@ -37,7 +36,8 @@ body {
 
 
 .flex-container {
-    margin: 30px;
+    width:100%;
+    margin: 30px 0 30px 0;
 	padding-bottom: 25px;
     display: flex;
     flex-wrap: wrap;
@@ -54,9 +54,37 @@ body {
     background-color: white;
     color: black;
     width: 350px;
-    margin: 20px;
+    margin: 25px;
     line-height: 10px;
     font-size: 15px;
+}
+
+.topp-nyhets-boks {
+    border-bottom: solid #9985ad 20px;
+	/* #666699 #6699cc #9985ad   */
+    background-color: white;
+    height: 300px;
+    width: 100%;
+    margin: 0 0 50px 0;
+}
+.topp-nyhets-boks h1{
+	padding: 50px 0 0 0;
+	margin: 0px 0px 0px 30% ;
+	font-size: 45px;
+}
+.topp-nyhets-boks p{
+	line-height:1.5;
+	padding: 20px 0 0 0;
+	margin: 0px 0px 0px 31% ;
+
+}
+footer {
+	margin: 50px 0 0 0;
+	float: left;
+	height: 300px;
+	width: 100%;
+	background-color: #aaaaaa;
+
 }
 </style>
 </head>
@@ -67,7 +95,10 @@ body {
   include_once('../includes/header_innlogget.php');
   include_once('../includes/ikke_logget_inn.inc.php');
   ?>
-<div class="topp-boks"></div>
+<div class ="topp-nyhets-boks">
+	<h1>Lorem Ipsum</h1>
+	<p>Et quia saepe et rerum hic suscipit. Sequi eligendi consequuntur delectus. <br>In ipsum praesentium est voluptas laudantium quo. Quisquam et enim<br> aspernatur fuga ea error. Est error enim eos mollitia voluptas et est exercitationem.<br>Sequi est quo nulla qui ipsam fuga magnam.</p>
+</div>
 
 <!-- Denne delen vil senere erstattes med data hentet fra db'en -->
 <div class="center">
@@ -152,5 +183,6 @@ body {
     </div>
 </div>
 </div>
+<footer></footer>
 </body>
 </html>
