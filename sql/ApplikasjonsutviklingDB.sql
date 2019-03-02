2b4cd3118f7f797fd49f880d8f0e2f6e9a295a58
Sorensen <mr.sorensen.k@gmail.com>
2019-03-02 16:23:43 +0100

    Lagt til arrangement og deltagelse i sql-script Ikke testet.

--- sql/ApplikasjonsutviklingDB.sql
+++ sql/ApplikasjonsutviklingDB.sql
@@ -35,7 +35,7 @@ CREATE TABLE bio (
     CONSTRAINT bioPK PRIMARY KEY (brukerNavn, bio),
     CONSTRAINT biobrukerFK FOREIGN KEY (brukerNavn) REFERENCES bruker (brukerNavn)
 );
-
+-- Husk meg funksjonalitet
 CREATE TABLE token (
     brukerNavn VARCHAR(45) NOT NULL UNIQUE,
     token CHAR(128),
@@ -54,4 +54,22 @@ CREATE TABLE meldinger (
     CONSTRAINT meldingerPK PRIMARY KEY (avsender, mottaker, sendtTid),
     CONSTRAINT meldingerBrukerFK FOREIGN KEY (avsender) REFERENCES bruker (brukerNavn),
     CONSTRAINT meldingerBrukerFK2 FOREIGN KEY (mottaker) REFERENCES bruker (brukerNavn)
-    );
+);
+-- For arrangement
+CREATE TABLE arrangement (
+    arrangementId INT(5),
+    vert VARCHAR(45),
+    startTid DATETIME(),
+    sluttTid DATETIME(),
+    Beskrivelse VARCHAR(255)
+    CONSTRAINT arrangementPK PRIMARY KEY(arrangementId),
+    CONSTRAINT arrangementBrukerFK FOREIGN KEY(vert) REFERENCES bruker(brukerNavn)
+);
+CREATE TABLE arrangementDeltagelse (
+    arrangementId INT(5),
+    deltager VARCHAR(45),
+    CONSTRAINT arrangementDeltagelsePK PRIMARY KEY(arrangementId, deltager),
+    CONSTRAINT arrangementDeltagelseBrukerFK FOREIGN KEY(deltager) REFERENCES bruker(brukerNavn),
+    CONSTRAINT arrangementDeltagelseArrangementFK FOREIGN KEY(arrangementId) REFERENCES arrangement(arrangementId)
+);
+
