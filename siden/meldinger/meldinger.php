69d83f7a5071a109705dfb1deb2c6ad892598c34
lyse89 <simenlyse@gmail.com>
2019-03-07 17:40:55 +0100

    La til main.js for Javascript functioner

--- siden/meldinger/meldinger.php
+++ siden/meldinger/meldinger.php
@@ -2,9 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Meldinger</title>
+    <style media="screen">
+
+    </style>
   </head>
   <body>
-    
+    <form class="sende" action="sendmeldigner.inc.php" method="post">
+      <input type="text" name="mottaker" value="mottaker" placeholder="Mottaker">
+      <textarea name="name" rows="8" cols="80"></textarea>
+    </form>
+
   </body>
 </html>
