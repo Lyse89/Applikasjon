07868e573f78ae9caac1278d61b9d932616a2b04
Sorensen <mr.sorensen.k@gmail.com>
2019-04-12 18:01:32 +0200

    Lagt til fil for rollesjekk

--- siden/includes/rollesjekk.inc.php
+++ siden/includes/rollesjekk.inc.php
@@ -1,12 +1,18 @@
 <?php
-    //Denne siden er utviklet av Kristoffer Sorensen, siste gang endret 12.04.2019
-    //Denne include-siden er kontrollert av Kristoffer Sorensen, siste gang 12.04.2019
-    include("logg_inn_db.inc.php");
+//Denne siden er utviklet av Kristoffer Sorensen, siste gang endret 12.04.2019
+//Denne include-siden er kontrollert av Kristoffer Sorensen, siste gang 12.04.2019
+//
+//include("logg_inn_db.inc.php");
+include("../includes/init.php");
+include("../includes/logg_inn_db.inc.php");
+$db = new PDO($dsn,"$dbBrukernavn","$dbPassord");
 
-    $stmt = $db->query('SELECT rolle FROM bruker WHERE brukerNavn ='.$_SESSION['brukernavn'].';')
+$sporring = 'SELECT * FROM bruker WHERE brukerNavn =\''.$_SESSION['brukernavn'].'\';';
+$stmt = $db->query($sporring);
 
-    if($stmt->rowCount()){
-        $row = $stmt->fetch();
+if($stmt->rowCount()){
+    while($row = $stmt->fetch()){
         $_SESSION['rolle'] = $row['rolle'];
-    } else {$_SESSION['rolle'] = '';}
+    }
+} else {$_SESSION['rolle'] = '';}
 ?>
