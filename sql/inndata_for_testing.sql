941c003510b6dc4401b89bcaa61d399f4609f636
PastaSauce <willir0407@gmail.com>
2019-04-12 18:24:36 +0200

    a

--- sql/inndata_for_testing.sql
+++ sql/inndata_for_testing.sql
@@ -89,6 +89,7 @@ select * from utestengt;
 select * from anmerkning;
 INSERT INTO utestengt (brukerNavn, gittAv, tid) VALUES
 ("q", "q", now());
+
 INSERT INTO anmerkning (brukerNavn, tid, forklaring) VALUES
 ("q", now(), "Dette er en forklaring på anmerkningen");
 
