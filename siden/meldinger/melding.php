a155c19d5d833a959f178d1d915a83e88cfe89fa
lyse89 <simenlyse@gmail.com>
2019-05-03 07:57:48 +0200

    Startet sende melding script

--- siden/meldinger/melding.php
+++ siden/meldinger/melding.php
@@ -4,13 +4,13 @@
 //Denne siden er kontrollert av Simen Lyse, siste gang 02.05.2019
 
 // Sjekk for om brukeren er innlogget og videresending til innlogget forside
-    include_once('../includes/ikke_logget_inn.inc.php');
+include_once('../includes/ikke_logget_inn.inc.php');
 ?>
 
 <html lang="en" dir="ltr">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="siden/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <meta charset="utf-8">
     <title>Meldinger</title>
     <style>
@@ -26,8 +26,7 @@ include_once('../includes/header_innlogget.php');
 
 <form class="sendMelding" action="send.inc.php" method="POST" onsubmit="sjekkSubmit();">
   <fieldset>
-      <input type="email" placeholder="Til" name="til" id="til" class="inputBoks" onchange="sjekkFelt()" autofocus>
-      <input type="email" placeholder="Fra" name="fra" id="fra" class="inputBoks" onchange="sjekkFelt()">
+      <input type="text" placeholder="Til" name="til" id="til" class="inputBoks" onchange="sjekkFelt()" autofocus>
       <input type="text" placeholder="Titel" name="subj" id="subj" class="inputTitel" onchange="sjekkFelt()">
       <textarea type="text" placeholder="Tekst...." name="meld" id="meld" class="inputText"></textarea>
       <input type="submit" value="Send" name="send" id="send" class="sndBtn">
--- siden/meldinger/send.inc.php
+++ siden/meldinger/send.inc.php
@@ -1,3 +1,8 @@
 <?php
+include_once("../includes/init.php");
+$db = new PDO($dsn,$dbBrukernavn,$dbPassord);
+
+$sql = "insert into interessekobling (brukerNavn,interesse)";
+$sql.= "values (:studentid,:interesser)";
 
 ?>
