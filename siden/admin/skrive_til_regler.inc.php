8d37ceac9d10522cd9c35b97893cbbc17a7cb987
Simen Lyse <simenlyse@gmail.com>
2019-06-04 23:56:19 +0200

    Updatert regler

--- siden/admin/skrive_til_regler.inc.php
+++ siden/admin/skrive_til_regler.inc.php
@@ -1,5 +1,7 @@
 <?php
 
+if(isset($_POST['btnRegel'])) {
+
   $regel0 = $_POST['regel0'];
   $regel1 = $_POST['regel1'];
   $regel2 = $_POST['regel2'];
@@ -14,20 +16,21 @@
 
 
   $handle = fopen("../regler/regler.html",'w');
-  fwrite($handle,"<html><head><title>Regler</title></head>");
-  fwrite($handle,"<body><p>1. ", "'$regel0';", "</p>");
-  fwrite($handle,"<p>2." , "'$regel1';", "</p>");
-  fwrite($handle,"<p>3." , "'$regel2';", "</p>");
-  fwrite($handle,"<p>4." , "'$regel3';", "</p>");
-  fwrite($handle,"<p>5." , "'$regel4';", "</p>");
-  fwrite($handle,"<p>6." , "'$regel5';", "</p>");
-  fwrite($handle,"<p>7." , "'$regel6';", "</p>");
-  fwrite($handle,"<p>8." , "'$regel7';", "</p>");
-  fwrite($handle,"<p>9." , "'$regel8';", "</p>");
-  fwrite($handle,"<p>10." ,"'$regel9';", "</p>");
-  fwrite($handle,"</body></html>");
+  fwrite($handle,"<html>\n\t<head><title>Regler</title></head>");
+  fwrite($handle,"\t<body><p>1. ", "'$regel0';", "</p>");
+  fwrite($handle,"\t<p>2. $regel1 </p> \n");
+  fwrite($handle,"\t<p>3. $regel2 </p> \n");
+  fwrite($handle,"\t<p>4. $regel3 </p> \n");
+  fwrite($handle,"\t<p>5. $regel4 </p> \n");
+  fwrite($handle,"\t<p>6. $regel5 </p> \n");
+  fwrite($handle,"\t<p>7. $regel6 </p> \n");
+  fwrite($handle,"\t<p>8. $regel7 </p> \n");
+  fwrite($handle,"\t<p>9. $regel8 </p> \n");
+  fwrite($handle,"\t<p>10. $regel9 </p> \n");
+  fwrite($handle,"\t</body></html>");
   fclose($handle);
 
 
   header("Location: admin_hovedside.php");
+}
 ?>
