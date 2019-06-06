a196e3a0a5dd87a030e520b975ddbada63acc69a
Simen Lyse <simenlyse@gmail.com>
2019-06-06 21:35:28 +0200

    Send email som admin

---
+++ siden/admin/sendemail.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+  ini_set("SMTP","s120.hbv.no");
+  ini_set("smtp_port","25");
+	date_default_timezone_set("Europe/Oslo");
+	$til = $_POST['til'];
+	$fra = 'admin@alumni.no';
+	$subject = $_POST['subj'];
+	$melding = $_POST['meld'];
+	$headers = "From: " . $fra . "\r\n" .
+				'X-Mailer: PHP/' . phpversion() . "\r\n" .
+				"MIME-Version: 1.0\r\n" .
+				"Content-Type: text/html; charset=utf-8\r\n" .
+				"Content-Transfer-Encoding: 8bit\r\n\r\n";
+
+?>
