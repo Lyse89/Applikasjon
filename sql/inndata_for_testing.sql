9920395429006606f7776b397a43c0787ce1bbb5
Sorensen <mr.sorensen.k@gmail.com>
2019-06-07 17:32:44 +0200

    Endret navn bilder. Fra id til tittel :(

--- sql/inndata_for_testing.sql
+++ sql/inndata_for_testing.sql
@@ -32,12 +32,12 @@ INSERT INTO bio (brukerNavn, bio) VALUES
 
 
 -- Inndata for eventer
-insert into arrangement(tittel, vert, lokasjon,startTid, sluttTid, Beskrivelse) values
-('Motivasjonsforedrag i vestre fløy', 'arne', 'Lokasjon for arr2', now(), now(), 'Velkommen til motivasjonsforedrag. Dette er beskrivelsen av arrangementet. Vi kan by på pizza og kaker til påmeldte.'),
-('Hackathon', 'bjarne', 'Lokasjon for arr1', now(), now(), 'Vel møtt til hackathon, arrangementet starter i det lille auditoriet. Oppgavene slippes klokken 7 og før dette vil deltagere deles opp i grupper for brainstorming.'),
-('Lan 23-25 mars', 'carl', 'Lokasjon for arr3', now(), now(), 'Debug inviterer til lan i workshop rommet. Det vil bli avholdt konkurranser i de spillene som stemmer frem. Det vil bli servert gratis pizza på fredag kveld.'),
-('Quiz - Jokern', 'donald', 'Lokasjon for arr4', now(), now(), 'Imorgen blir det quiz på Jokern - Ringerike Studentkro kl 20, husk det blir premier! Trykk skal hvis dere vet dere kommer! Gledes til å se dere imorgen!'),
-('10 års jubileum', 'e', 'Lokasjon for arr5', now(), now(), 'Beskrivelse av arrangementet5');
+insert into arrangement(tittel, vert, lokasjon,startTid, sluttTid, Beskrivelse, bilde) values
+('Motivasjonsforedrag i vestre fløy', 'arne', 'Lokasjon for arr2', now(), now(), 'Velkommen til motivasjonsforedrag. Dette er beskrivelsen av arrangementet. Vi kan by på pizza og kaker til påmeldte.','Motivasjonsforedrag i vestre fløy.png'),
+('Hackathon', 'bjarne', 'Lokasjon for arr1', now(), now(), 'Vel møtt til hackathon, arrangementet starter i det lille auditoriet. Oppgavene slippes klokken 7 og før dette vil deltagere deles opp i grupper for brainstorming.', 'Hackathon.png'),
+('Lan 23-25 mars', 'carl', 'Lokasjon for arr3', now(), now(), 'Debug inviterer til lan i workshop rommet. Det vil bli avholdt konkurranser i de spillene som stemmer frem. Det vil bli servert gratis pizza på fredag kveld.', 'Lan 23-25 mars'),
+('Quiz - Jokern', 'donald', 'Lokasjon for arr4', now(), now(), 'Imorgen blir det quiz på Jokern - Ringerike Studentkro kl 20, husk det blir premier! Trykk skal hvis dere vet dere kommer! Gledes til å se dere imorgen!', 'Quiz - Jokern'),
+('10 års jubileum', 'e', 'Lokasjon for arr5', now(), now(), 'Beskrivelse av arrangementet5', '10 års jubileum.png');
 
 
 -- arrangementDeltagelse
